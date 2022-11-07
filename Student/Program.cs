622bc5f4cc33ce7113455c2626bf214291a7291f
Sadixzadel <98652872+Sadixzadel@users.noreply.github.com>
2022-11-07 23:26:31 +0400

    Update Program.cs

--- Student/Program.cs
+++ Student/Program.cs
@@ -8,9 +8,9 @@ internal class Program
     private static void Main(string[] args)
     {
 
-        Student student = new Student("Nurlan", "Huseynov", "Group1", 4.5);
+        Student student = new Student("Nurlan", "Huseynov", "Group1", 4.9);
         Aspirant aspiranti = new Aspirant("Leyla", "Sadikhzade", "Group2", 5, "Work1");
-        Student aspirant2 = new Aspirant("Rustam", "Minazov", "Group3", 4.8, "Workz");
+        Student aspirant2 = new Aspirant("Rustam", "Minazov", "Group3", 3, "Workz");
 
     } 
 }
