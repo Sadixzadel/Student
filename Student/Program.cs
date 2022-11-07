c539377e01e69a22047b9079d631b980ac10a70f
HP <HP@DESKTOP-EQ9MNTC>
2022-11-07 23:19:46 +0400

    Добавьте файлы проекта.

---
+++ Student/Program.cs
@@ -0,0 +1,16 @@
+﻿using Student1;
+using System.Xml.Linq;
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Student student = new Student("Nurlan", "Huseynov", "Group1", 4.5);
+        Aspirant aspiranti = new Aspirant("Leyla", "Sadikhzade", "Group2", 5, "Work1");
+        Student aspirant2 = new Aspirant("Rustam", "Minazov", "Group3", 4.8, "Workz");
+
+    } 
+}
---
+++ Student/Student.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Xunit;
+
+namespace Student1;
+
+public class Student
+{
+    public string FirstName;
+    public string LastName;
+    public string Group;
+    public double AverageMark;
+
+    public Student(string firstName, string lastName, string group, double averageMark)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Group = group;
+        AverageMark = averageMark;
+    }
+
+    public double GetScholarship()
+    {
+        if (AverageMark == 5)
+        {
+            return 100;
+        }
+        else
+        {
+            return  80;
+        }
+        
+    }
+}
+public class Aspirant : Student
+{
+    public string ScientificWork { get; set; }
+    public Aspirant(string firstName, string lastName, string group, double averageMark, string scientificWork) : base(firstName, lastName, group, averageMark)
+    {
+        ScientificWork = scientificWork;
+    }
+
+    public new double GetScholarship()
+    {
+        if (AverageMark == 5)
+        {
+            return 100;
+        }
+        else
+        {
+            return 80;
+        }
+
+    }
+}
+
